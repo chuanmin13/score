9d8ba351da98d27922c159f5d704c29dd1b1fe8f
Jamie Liao <jamie.liao@alp.com.tw>
2023-06-20 17:45:45 +0800

    sw test

--- sw.js
+++ sw.js
@@ -16,7 +16,53 @@ this.addEventListener('install', event => {
 })
 
 this.addEventListener("fetch", event => {
-  onsole.log('Handling fetch event for', event.request.url);
+  console.log('Handling fetch event for', event.request.url);
   
-  event.respondWith(caches.match(event.request))
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          alert('Useing Catch!')
+          return response
+        } 
+
+      return fetch(event.request)
+        .then(function (response) {
+          console.log('Response from network is:', response)
+          // 加入快取供之後使用
+          return caches.open('v1').then(function (cache) {
+            cache.put(event.request, response.clone())
+            return response;
+          })
+        })
+        .catch(function (error) {
+          // 錯誤處理
+          console.error('Fetching failed:', error)
+          throw error;
+        })
+      
+    })
+  )
 })
+
+this.addEventListener('message', function (e) {
+  // e.source is a client object
+  e.source.postMessage('Hello! Your message was: ' + e.data);
+});
+
+function sendMessage(message) {
+  console.log('Send message...');
+  return new Promise(function (resolve, reject) {
+    var messageChannel = new MessageChannel();
+    messageChannel.port1.onmessage = function (event) {
+      if (event.data.error) {
+        reject(event.data.error);
+      } else {
+        resolve(event.data);
+      }
+    };
+    navigator.serviceWorker.controller.postMessage(message, [messageChannel.port2]);
+  });
+}
+
+sendMessage('Hello World!');
